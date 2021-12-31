3eff2e2f56005c48af998f728b1a1547d1f72900
Luis Bartel <luis@bartelluis.de>
2021-12-31 01:19:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = "your bot token";
+exports.TOKEN = "OTI2MjY3NDQ3OTc0MTc4ODM2.Yc5LyA.loLyodH5rzSK5OUZ_1EdcpWzu4M";
 
-exports.PREFIX = 'YOUR PREFIX';
+exports.PREFIX = '!';
 
-exports.OWNER_ID = "YOUR ACC ID"
+exports.OWNER_ID = "769176314955038721"
